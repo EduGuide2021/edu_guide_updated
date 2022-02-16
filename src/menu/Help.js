3632de027b279f261d71b9652e80af034b092d80
hendrix034 <hendrixnagle@yahoo.com>
2022-02-16 18:25:49 +0800

    Update Help.js

--- src/menu/Help.js
+++ src/menu/Help.js
@@ -43,7 +43,7 @@ export default function Contact() {
           <form align="center">
             <label className="feed-back-label">
               <br></br>
-            Click this button to download instructions
+           click
               <br></br>
               <br></br>
               
